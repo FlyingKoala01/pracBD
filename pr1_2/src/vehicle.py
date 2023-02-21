b8c101ef9d4399ba30bd0eb11103eb32963fc98d
FlyingKoala01 <flyingkoala01@gmail.com>
2023-02-21 08:27:19 +0100

    Doctests vehicle.py

--- pr1_2/src/vehicle.py
+++ pr1_2/src/vehicle.py
@@ -45,7 +45,7 @@ class Vehicle:
     It is also used to ease the encoding/decoding of bytes/strings when storing and retrieving data from the db.
 
     """
-    
+
     BRAND_MAX_LENGTH = 10
     COLOR_MAX_LENGTH = 10
     LICENSE_PLATE_LENGTH = 7
@@ -53,6 +53,8 @@ class Vehicle:
     def __init__(self, license_plate, color, brand) -> None:
         """
         Attributes can be bytes or string
+
+        >>> car = Vehicle("1234ABC", "Red", "BMW")
         """
         self.license_plate = license_plate if type(license_plate) == str else license_plate.decode()
         self.color = color if type(color) == str else color.decode().strip('\x00')
@@ -61,7 +63,18 @@ class Vehicle:
     def validate(self) -> tuple:
         """
         Returns (bool, str) with (True, "Valid vehicle") or (False, "Reason").
+
+        >>> (Vehicle("1234ABC", "Red", "BMW")).validate()
+        (True, 'Valid vehicle')
+        >>> (Vehicle("123AABC", "Red", "BMW")).validate()
+        (False, 'Invalid license plate: use format 0000AAA')
+        >>> (Vehicle("1234ABC", "Reeeeeeeeeeed", "BMW")).validate()
+        (False, 'Car color should not exceed 10 characters.')
+        >>> (Vehicle("1234ABC", "Red", "BMWWWWWWWWWWWW")).validate()
+        (False, 'Car brand should not exceed 10 characters.')
+
         """
+
         if not valid_license_plate(self.license_plate):
             return (False, "Invalid license plate: use format 0000AAA")
         if len(self.color) > 10:
@@ -71,6 +84,12 @@ class Vehicle:
         return (True, "Valid vehicle")
     
     def __repr__(self) -> str:
+        """
+        
+        >>> car = Vehicle("1234ABC", "Red", "BMW")
+        >>> print(car)
+        Vehicle <License plate: 1234ABC, Brand: BMW, Color: Red>
+        """
         return f"Vehicle <License plate: {self.license_plate}, Brand: {self.brand}, Color: {self.color}>"
     
     def __str__(self) -> str:
